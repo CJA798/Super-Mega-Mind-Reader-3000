31ac99be553cb83770a210cb59594fc027fc7ef4
Carlos A <cjacjacja@hotmail.com>
2024-04-05 04:14:23 -0400

    Added collapsing headers to data container and improved functionality

---
+++ src/data/dataset_handler.py
@@ -0,0 +1,6 @@
+class DatasetHandler():
+    def __init__(self) -> None:
+        self.raw_dataset_path = None
+        self.train_dataset_path = None
+        self.validation_dataset_path = None
+        self.test_dataset_path = None
--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,5 +1,7 @@
 import dearpygui.dearpygui as dpg
 from os import path
+from data.dataset_handler import DatasetHandler
+import os
 class GUI:
     def __init__(self):
         # Initialize the dearpygui context
@@ -8,10 +10,10 @@ class GUI:
 
         self.viewport_width = 800
         self.viewport_height = 600
+        self.icon_path = os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "img"), "icon.ico")
 
-        self.dataset_path = None
-        self.dataset_name = None
-        self.model_path = None
+        # Initialize the dataset handler
+        self.dataset_handler = DatasetHandler()
         
     def setup_gui(self):
         self.add_data_container()
@@ -19,7 +21,9 @@ class GUI:
         self.add_output_container()
         dpg.create_viewport(title="Super Mega Mind Reader 3000",
                             width=self.viewport_width,
-                            height=self.viewport_height)
+                            height=self.viewport_height,
+                            small_icon=self.icon_path,
+                            large_icon=self.icon_path)
         
 
     def add_data_container(self):
@@ -40,24 +44,73 @@ class GUI:
                 None
             '''
             # Update the dataset path
-            self.dataset_path = app_data['file_path_name']
+            self.dataset_handler.raw_dataset_path = app_data['file_path_name']
 
-            # Update the dataset name
-            self.dataset_name = path.basename(self.dataset_path)
-            
             # Update the current dataset name displayed on the data container
-            dpg.set_value("current_dataset_name", f"Current dataset: {self.dataset_name}")
+            dpg.set_value("current_raw_dataset_name", f"Raw Dataset: {path.basename(self.dataset_handler.raw_dataset_path)}")
         
-        def preprocess_data_cb():
-            print("Preprocessing data")
+        def folder_dialog_cb(sender: str, app_data: dict)->None:
+            '''
+            Callback function for the folder dialog in the data container.
 
-        def collect_data_cb():
-            print("Collecting data")
+            This function updates the dataset path and name when a file is selected, and updates the current dataset name displayed on the data container.
 
-        with dpg.file_dialog(directory_selector=False, show=False, callback=file_dialog_cb, tag="data_file_dialog_tag", width=700 ,height=400):
-            #dpg.add_file_extension(".*")
+            args:
+                sender (str): The sender of the callback.
+                app_data (dict): The data passed to the callback.
+
+            returns:
+                None
+
+            raises:
+                ValueError: If an invalid dataset type is loaded.
+            '''
+            # Update the respective dataset path and name displayed on the data container's info tab
+            if dpg.get_value("load_radio_button") == "Train":
+                self.dataset_handler.train_dataset_path = app_data['file_path_name']
+                dpg.set_value("current_train_dataset_name", f"Train Dataset: {path.basename(self.dataset_handler.train_dataset_path)}")
+
+            elif dpg.get_value("load_radio_button") == "Test":
+                self.dataset_handler.test_dataset_path = app_data['file_path_name']
+                dpg.set_value("current_test_dataset_name", f"Test Dataset: {path.basename(self.dataset_handler.test_dataset_path)}")
+                
+            elif dpg.get_value("load_radio_button") == "Valid":
+                self.dataset_handler.validation_dataset_path = app_data['file_path_name']
+                dpg.set_value("current_validation_dataset_name", f"Validation Dataset: {path.basename(self.dataset_handler.validation_dataset_path)}")
+
+            else:
+                raise ValueError("Invalid dataset type loaded")
+
+        def load_data_cb(sender: str, app_data: dict)->None:
+            '''
+            Callback function for the load data button in the data container.
+            This function shows the respective file dialog based on the selected dataset type.
+
+            args:
+                sender (str): The sender of the callback.
+                app_data (dict): The data passed to the callback.
+
+            returns:
+                None
+
+            raises:
+                ValueError: If an invalid dataset type is selected.
+            '''
+            # Show the respective file dialog based on the selected dataset type
+            if dpg.get_value("load_radio_button") == "Raw":
+                dpg.show_item("load_data_file_dialog")
+            elif dpg.get_value("load_radio_button") == "Train" or dpg.get_value("load_radio_button") == "Test" or dpg.get_value("load_radio_button") == "Valid":
+                dpg.show_item("load_data_folder_dialog")
+            else:
+                raise ValueError("Invalid dataset type selected")
+
+
+        with dpg.file_dialog(directory_selector=False, show=False, callback=file_dialog_cb, tag="load_data_file_dialog", width=700 ,height=400):
             dpg.add_file_extension("", color=(150, 150, 255, 255))
             dpg.add_file_extension(".csv", color=(150, 255, 150, 255))
+
+        with dpg.file_dialog(directory_selector=True, show=False, callback=folder_dialog_cb, tag="load_data_folder_dialog", width=700 ,height=400):
+            dpg.add_file_extension("", color=(150, 150, 255, 255))
 
         with dpg.window(label="Data",
                         width=self.viewport_width//3,
@@ -65,10 +118,36 @@ class GUI:
                         no_close=True,
                         no_move=True,
                         no_resize=True):
-            dpg.add_button(label="Load", callback=lambda: dpg.show_item("data_file_dialog_tag"), tag="load_data_button")
-            dpg.add_button(label="Preprocess", callback=preprocess_data_cb, tag="preprocess_data_button")
-            dpg.add_button(label="Collect", callback=collect_data_cb, tag="collect_data_button")
-            dpg.add_text(f"Current dataset: {self.dataset_name}", label="output_text", tag="current_dataset_name", wrap=self.viewport_width//4)
+            
+            def _log(sender, app_data):
+                pass
+            with dpg.collapsing_header(label="Load"):
+                dpg.add_button(label="Load", callback=load_data_cb, tag="load_data_button")
+    
+                with dpg.group(horizontal=True):
+                    dpg.add_radio_button(("Raw", "Train", "Test", "Valid"), horizontal=True, default_value="Raw", tag="load_radio_button")
+            
+            with dpg.collapsing_header(label="Preprocess"):
+                dpg.add_button(label="Preprocess", callback=_log)
+                with dpg.group(horizontal=True):
+                    dpg.add_checkbox(label="A", callback=_log, default_value=True)
+                    dpg.add_checkbox(label="B", callback=_log, default_value=True)
+                    dpg.add_checkbox(label="C", callback=_log, default_value=True)
+                    dpg.add_checkbox(label="D", callback=_log, default_value=True)
+            
+            
+            with dpg.collapsing_header(label="Info", default_open=True):
+                raw_dataset_name = path.basename(self.dataset_handler.raw_dataset_path) if self.dataset_handler.raw_dataset_path else None
+                train_dataset_name = path.basename(self.dataset_handler.train_dataset_path) if self.dataset_handler.train_dataset_path else None
+                test_dataset_name = path.basename(self.dataset_handler.test_dataset_path) if self.dataset_handler.test_dataset_path else None
+                validation_dataset_name = path.basename(self.dataset_handler.validation_dataset_path) if self.dataset_handler.validation_dataset_path else None
+
+                dpg.add_text(f"Raw Dataset: {raw_dataset_name}", label="raw_dataset_text", wrap=self.viewport_width//4, tag="current_raw_dataset_name")
+                dpg.add_text(f"Train Dataset: {train_dataset_name}", label="train_dataset_text", wrap=self.viewport_width//4, tag="current_train_dataset_name")
+                dpg.add_text(f"Test Dataset: {test_dataset_name}", label="test_dataset_text", wrap=self.viewport_width//4, tag="current_test_dataset_name")
+                dpg.add_text(f"Validation Dataset: {validation_dataset_name}", label="validation_dataset_text", wrap=self.viewport_width//4, tag="current_validation_dataset_name")
+
+
 
     def add_model_container(self):
         def callback(sender, app_data):
